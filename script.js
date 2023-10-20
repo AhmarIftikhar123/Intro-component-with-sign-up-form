96dee9cafb510e717a9ce2791addcf5e2091f8c0
AhmarIftikhar123 <140446129+AhmarIftikhar123@users.noreply.github.com>
2023-10-20 21:36:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,117 @@
-window.addEventListener("DOMContentLoaded", () => {
-  let change_win_img = () => {
-    let window_Width = window.innerWidth;
-    let body = document.querySelector("body");
-    let new_img_url =
-      window_Width < 375
-        ? "images/bg-intro-mobile.png"
-        : "images/bg-intro-desktop.png";
+// All input variables
+const first_name = document.getElementById("first_name");
+const password = document.getElementById("password");
+const email = document.getElementById("email");
+const txt_box = document.getElementById("txt_area");
 
-    body.style.backgroundImage = `url(${new_img_url})`;
-  };
+// All Error variables
 
-  change_win_img();
+const name_error = document.getElementById("name_error");
+const password_error = document.getElementById("password_error");
+const email_error = document.getElementById("email_error");
+const txt_error = document.getElementById("txt_error");
 
-  window.addEventListener('resize', ()=>{
-    change_win_img();
-  })
+// submit_btn
 
-  let first_name = document.getElementById("first_name");
+const submit_btn = document.getElementById("submit_btn");
 
-  let last_name = document.getElementById("last_name");
+// For showing and hiding password
 
-  let email = document.getElementById("email");
+const Show_hide_btn = document.getElementById("show_hide_btn");
 
-  let password = document.getElementById("password");
+Show_hide_btn.onclick = (e) => {
+  e.preventDefault();
+  if (password.type === "password") {
+    password.type = "text";
+    Show_hide_btn.classList.replace("fa-eye-slash", "fa-eye");
+  } else {
+    password.type = "password";
+    Show_hide_btn.classList.replace("fa-eye", "fa-eye-slash");
+  }
+};
 
-  let form = document.querySelector("form");
+// Name validation
 
-  form.addEventListener("submit", (eve) => {
-    let isValid = true;
-
-    if (!first_name.value.trim()) {
-      displayerror(first_name, "First Name can't be empty");
-      isValid = false;
-    } else {
-      hideerror(first_name);
-    }
-    if (!last_name.value.trim()) {
-      displayerror(last_name, "Last Name can't be empty");
-      isValid = false;
-    } else {
-      hideerror(last_name);
-    }
-
-    if (!isValidEmail(email.value.trim())) {
-      displayerror(email, "Looks like this is not a valid email");
-      isValid = false;
-    } else {
-      hideerror(email);
-    }
-
-    if (password.value == "") {
-      displayerror(password, "Password can't be empty");
-      isValid = false;
-    } else if (password.value.length < 8) {
-      displayerror(password, "Password must at-least 8 Characters");
-      isValid = false;
-    } else {
-      hideerror(password);
-    }
-
-    if (!isValid) {
-      eve.preventDefault();
-    }
-  });
-
-  let displayerror = (inputelement, errormsg) => {
-    let smallElement = inputelement.nextElementSibling;
-    smallElement.style.display = "flex";
-    smallElement.style.color = "red";
-    smallElement.textContent = errormsg;
-  };
-
-  let hideerror = (inputelement) => {
-    let smallElement = inputelement.nextElementSibling;
-    smallElement.textContent = "";
-    smallElement.style.display = "none";
-  };
-
-  let isValidEmail = (email) => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-  };
+let validator_name = "";
+first_name.addEventListener("input", () => {
+  let regex = /^[A-Za-z]+\s[A-Za-z]+$/;
+  if (first_name.value === "") {
+    name_error.textContent = "";
+    name_error.style.visibility = "hidden";
+    first_name.nextElementSibling.style.visibility = "hidden";
+    return (validator_name = false);
+  } else if (!first_name.value.match(/[A-Za-z]+ /)) {
+    name_error.textContent = "Valid Name Required";
+    name_error.style.visibility = "visible";
+    first_name.nextElementSibling.style.visibility = "visible";
+    return (validator_name = false);
+  } else if (!first_name.value.match(regex)) {
+    name_error.textContent = "Full Name Required";
+    name_error.style.visibility = "visible";
+    first_name.nextElementSibling.style.visibility = "visible";
+    return (validator_name = false);
+  } else if (first_name.value.match(regex)) {
+    name_error.textContent = "";
+    name_error.style.visibility = "hidden";
+    first_name.nextElementSibling.style.visibility = "hidden";
+    return (validator_name = true);
+  }
 });
+
+first_name.addEventListener("blur", () => {
+  name_error.textContent = "";
+  name_error.style.visibility = "hidden";
+  first_name.nextElementSibling.style.visibility = "hidden";
+});
+
+var valid_email = "";
+email.addEventListener("input", () => {
+  let regex = /[A-Za-z0-9]+\@[a-z]{5}\.[a-z]{3,}/;
+  if (email.value === "") {
+    email_error.textContent = "";
+    email_error.style.visibility = "hidden";
+    email.nextElementSibling.style.visibility = "hidden";
+    return (valid_email = false);
+  }
+  if (!email.value.match(regex)) {
+    email_error.textContent = "Valid Email Required";
+    email_error.style.visibility = "visible";
+    email.nextElementSibling.style.visibility = "visible";
+    return (valid_email = false);
+  } else {
+    email_error.textContent = "";
+    email_error.style.visibility = "hidden";
+    email.nextElementSibling.style.visibility = "hidden";
+    return (valid_email = true);
+  }
+});
+
+email.addEventListener("blur", () => {
+  email_error.textContent = "";
+  email_error.style.visibility = "hidden";
+  email.nextElementSibling.style.visibility = "hidden";
+});
+
+var valid_txt = "";
+txt_box.addEventListener("input", () => {
+  let len = 30;
+  let left_msg = len - txt_box.value.length;
+  if (left_msg > 0) {
+    txt_error.textContent = `${left_msg} More word Required`;
+    txt_error.style.visibility = "visible";
+    txt_box.nextElementSibling.style.visibility = "visible";
+    return (valid_txt = false);
+  } else {
+    txt_error.style.visibility = "hidden";
+    txt_error.textContent = ``;
+    email.nextElementSibling.style.visibility = "hidden";
+    return (valid_txt = true);
+  }
+});
+
+submit_btn.onclick = (e) => {
+  if (valid_email !== true || validator_name !== true || valid_txt !== true) {
+    e.preventDefault();
+    alert("Fill all inputs with Rules");
+  }
+};
